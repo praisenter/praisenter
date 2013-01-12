9a788b100098ac91632be168afd8a0d84bea8176
wjbittle@gmail.com <wjbittle@gmail.com@1ab9f8e5-4cd0-d5b3-4e2c-6b8dcbc406cb>
2013-01-12 19:58:53 +0000

    Fixed transitions to be pixel perfect; small bug fixes; added fixmes for other bugs

--- src/org/praisenter/transitions/HorizontalSplitCollapse.java
+++ src/org/praisenter/transitions/HorizontalSplitCollapse.java
@@ -40,21 +40,33 @@ public class HorizontalSplitCollapse extends Transition {
 	@Override
 	public void render(Graphics2D g2d, BufferedImage image0, BufferedImage image1, double pc) {
 		Shape shape = g2d.getClip();
+		// to get pixel perfect results we need to make sure we use the y and height from
+		// image0 so that image1 will be clipped appropriately (this is only necessary when
+		// we have both image0 and image1)
+		int y0 = 0;
+		int h0 = 0;
 		if (image0 != null) {
 			int hh = image0.getHeight() / 2;
-			int y = (int)Math.ceil((double)hh * pc);
-			int h = (int)Math.ceil(image0.getHeight() * (1.0 - pc));
-			g2d.setClip(0, y, image0.getWidth(), h);
+			y0 = (int)Math.ceil((double)hh * pc);
+			h0 = (int)Math.ceil(image0.getHeight() * (1.0 - pc));
+			g2d.setClip(0, y0, image0.getWidth(), h0);
 			g2d.drawImage(image0, 0, 0, null);
 		}
 		if (this.type == Transition.Type.IN && image1 != null) {
+			int w = image1.getWidth();
+			int h = 0;
+			int y = 0;
 			// create two rectangles and merge them into one area for the clip
 			int hh = image1.getHeight() / 2;
-			// h = hh - hh * pc
-			int h = (int)Math.ceil((double)hh * pc);
-			int w = image1.getWidth();
-			// y = hh + hh * pc
-			int y = (int)Math.ceil((double)image1.getHeight() - (double)hh * pc);
+			if (image0 != null) {
+				h = y0;
+				y = y0 + h0;
+			} else {
+				// h = hh - hh * pc
+				h = (int)Math.ceil((double)hh * pc);
+				// y = hh + hh * pc
+				y = (int)Math.ceil((double)image1.getHeight() - (double)hh * pc);
+			}
 			Rectangle left = new Rectangle(0, 0, w, h);
 			Rectangle right = new Rectangle(0, y, w, h);
 			Area area = new Area();
