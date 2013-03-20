18134739e5f6f46691020e847a5e41ff6ebd78dd
wjbittle@gmail.com <wjbittle@gmail.com@1ab9f8e5-4cd0-d5b3-4e2c-6b8dcbc406cb>
2013-03-20 00:55:33 +0000

    Added more system properties to the system dialog

--- src/org/praisenter/application/Version.java
+++ src/org/praisenter/application/Version.java
@@ -27,7 +27,7 @@ package org.praisenter.application;
 /**
  * The version of the application.
  * @author William Bittle
- * @version 2.0.0
+ * @version 2.0.1
  * @since 1.0.0
  */
 public class Version {
@@ -38,7 +38,7 @@ public class Version {
 	private static final int MINOR = 0;
 	
 	/** The revision number; small impact changes; bug fixes, small enhancements, etc. */
-	private static final int REVISION = 0;
+	private static final int REVISION = 1;
 	
 	/**
 	 * Hide the constructor.
