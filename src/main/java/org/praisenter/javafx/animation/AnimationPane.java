db814aae41a0794ae8cc6b235fd1aa918685ae2e
William Bittle <wjbittle@gmail.com>
2016-03-26 09:24:39 -0400

    Meh

--- src/main/java/org/praisenter/javafx/animation/AnimationPane.java
+++ src/main/java/org/praisenter/javafx/animation/AnimationPane.java
@@ -25,10 +25,127 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 
 public final class AnimationPane extends BorderPane {
+	/** The configured animation */
+	private final ObjectProperty<SlideAnimation> animation = new SimpleObjectProperty<SlideAnimation>() {
+		public void set(SlideAnimation animation) {
+			if (paint instanceof LinearGradient) {
+	    		LinearGradient lg = (LinearGradient)paint;
+	    		
+	    		final double x1 = clamp(lg.getStartX(), 0, 1);
+	    		final double y1 = clamp(lg.getStartY(), 0, 1);
+	    		final double x2 = clamp(lg.getEndX(), 0, 1);
+	    		final double y2 = clamp(lg.getEndY(), 0, 1);
+	    		
+	    		type.set(0);
+	    		rdoLinear.setSelected(true);
+	    		cycle.set(lg.getCycleMethod());
+	    		switch (lg.getCycleMethod()) {
+	    			case REFLECT:
+	    				rdoCycleReflect.setSelected(true);
+	    				break;
+	    			case REPEAT:
+	    				rdoCycleRepeat.setSelected(true);
+	    				break;
+					default:
+	    				rdoCycleNone.setSelected(true);	
+	    		}
+	    		handle1X.set(x1);
+	    		handle1Y.set(y1);
+	    		handle2X.set(x2);
+	    		handle2Y.set(y2);
+	    		
+	    		// stops
+	    		List<Stop> stops = lg.getStops();
+	    		if (stops != null && stops.size() > 0) {
+	    			Stop s1 = stops.get(0);
+	    			stop1.set(s1);
+	    			sldStop1.setValue(s1.getOffset());
+	    			pkrStop1.setValue(s1.getColor());
+	    			if (stops.size() > 1) {
+	    				Stop s2 = stops.get(1);
+	    				stop2.set(s2);
+	    				sldStop2.setValue(s2.getOffset());
+	    				pkrStop2.setValue(s2.getColor());
+	    			}
+	    		}
+	    		
+	    		// set the handle locations
+	    		handle1.setLayoutX(x1 * preview.getWidth());
+	    		handle1.setLayoutY(y1 * preview.getHeight());
+	    		handle2.setLayoutX(x2 * preview.getWidth());
+	    		handle2.setLayoutY(y2 * preview.getHeight());
+	    	} else if (paint instanceof RadialGradient) {
+	    		RadialGradient rg = (RadialGradient)paint;
+	    		
+	    		final double sqrt2 = Math.sqrt(2.0);
+	    		final double r = clamp(rg.getRadius(), 0, 1);
+	    		final double x1 = clamp(rg.getCenterX(), 0, 1);
+	    		final double y1 = clamp(rg.getCenterY(), 0, 1);
+	    		final double x2 = clamp(x1 + sqrt2 * r, 0, 1);
+	    		final double y2 = clamp(y1 + sqrt2 * r, 0, 1);
+	    		
+	    		type.set(1);
+	    		rdoRadial.setSelected(true);
+	    		cycle.set(rg.getCycleMethod());
+	    		switch (rg.getCycleMethod()) {
+	    			case REFLECT:
+	    				rdoCycleReflect.setSelected(true);
+	    				break;
+	    			case REPEAT:
+	    				rdoCycleRepeat.setSelected(true);
+	    				break;
+    				default:
+	    				rdoCycleNone.setSelected(true);	
+	    		}
+	    		handle1X.set(x1);
+	    		handle1Y.set(y1);
+	    		handle2X.set(x2);
+	    		handle2Y.set(y2);
+	    		
+	    		// we have to recompute the radius since the radius could
+	    		// be bigger than what we allow (for example if the center
+	    		// is at (0.5, 0.5) the max radius is sqrt(0.5) instead of 1
+	    		final double d1 = x2 - x1;
+	    		final double d2 = y2 - y1;
+	    		radius.set(Math.sqrt(d1 * d1 + d2 * d2));
+	    		
+	    		// stops
+	    		List<Stop> stops = rg.getStops();
+	    		if (stops != null && stops.size() > 0) {
+	    			Stop s1 = stops.get(0);
+	    			stop1.set(s1);
+	    			sldStop1.setValue(s1.getOffset());
+	    			pkrStop1.setValue(s1.getColor());
+	    			if (stops.size() > 1) {
+	    				Stop s2 = stops.get(1);
+	    				stop2.set(s2);
+	    				sldStop2.setValue(s2.getOffset());
+	    				pkrStop2.setValue(s2.getColor());
+	    			}
+	    		}
+	    		
+	    		// set the handle locations
+	    		handle1.setLayoutX(x1 * preview.getWidth());
+	    		handle1.setLayoutY(y1 * preview.getHeight());
+	    		handle2.setLayoutX(x2 * preview.getWidth());
+	    		handle2.setLayoutY(y2 * preview.getHeight());
+	    	}
+			
+			// doing this will mean that setting it to null or any other type of paint will do nothing
+			// since it will just use the current observables to generate a new paint
+			
+			// this has the added effect of allowing us to update the paint property without having
+			// it go through the conversion process above by calling: set(null);
+			super.set(createPaint());
+		}
+	};
 	
-	private final ObjectProperty<SlideAnimation> animation = new SimpleObjectProperty<SlideAnimation>();
+	// sub properties of the animation
 	
 	private final ObjectProperty<AnimationOption> animationOption = new SimpleObjectProperty<AnimationOption>();
 	private final ObjectProperty<AnimationOption> easingOption = new SimpleObjectProperty<AnimationOption>();
@@ -43,6 +160,17 @@ public final class AnimationPane extends BorderPane {
 	private final ObjectProperty<Direction> direction = new SimpleObjectProperty<Direction>();
 	private final ObjectProperty<ShapeType> shapeType = new SimpleObjectProperty<ShapeType>();
 	private final ObjectProperty<Operation> operation = new SimpleObjectProperty<Operation>();
+	
+	// nodes
+	
+	TextField txtDuration;
+	TextField txtDelay;
+	ChoiceBox<AnimationType> cbAnimationType;
+	ChoiceBox<EasingType> cbEasingType;
+	ChoiceBox<Orientation> cbOrientation;
+	ChoiceBox<Direction> cbDirection;
+	ChoiceBox<ShapeType> cbShapeType;
+	ChoiceBox<Operation> cbOperation;
 	
 	public AnimationPane() {
 		// setup the animation selection
