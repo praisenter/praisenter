3f55f97511f0931f67d3dfb3bead1f0bbf51c337
William Bittle <wjbittle@gmail.com>
2021-06-01 20:54:29 -0400

    Fixed issues with showing updated slides after edit

--- src/main/java/org/praisenter/data/configuration/Display.java
+++ src/main/java/org/praisenter/data/configuration/Display.java
@@ -121,7 +121,7 @@ public final class Display implements ReadOnlyDisplay, Copyable, Comparable<Disp
 	
 	@Override
 	public String toString() {
-		return "#" + this.id.get() + " " + this.role.get() + " (" + this.x.get() + "," + this.y.get() + ") " + this.width.get() + "x" + this.height.get();
+		return "#" + (this.id.get() + 1) + " " + this.role.get() + " (" + this.x.get() + "," + this.y.get() + ") " + this.width.get() + "x" + this.height.get();
 	}
 	
 	@Override
